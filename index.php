42967f2ed467ff66f6ba1f324c6fcb5851e32d77
Elogeek <elodiechristin@gmail.com>
2021-02-03 09:41:48 +0100

    strart and finish exo 168

--- index.php
+++ index.php
@@ -9,19 +9,44 @@
 
 $to = [
     'j.conan@fondationface.org',
-    ...
+    'elodiechristin@gmail.com'
 ];
 
-$html = '
-    <html lang="fr">
-        <head>
-            <title>Mes actualités</title>
-        </head>
-        <body>
-            <div>
-                <!-- Vos actualités ici -->
-            </div>
-        </body>
-    </html>
-';
+$html = "Coucou, merci de t'être abonné à ma page Travel world ! Voici tes cadeaux de bienvenue !"
+?>
 
+<html lang="fr">
+    <head>
+        <title>Mes actualités</title>
+    </head>
+    <body>
+        <div>
+            Dans la prochaine newsletter, tu recevras un voyage gratos!
+            <!-- Vos actualités ici -->
+        </div>
+    </body>
+</html>
+
+<?php
+$subject = "thank you for following me";
+$headers = array (
+    'Reply-To' => 'Travelworld@gmail.com',
+    'X-Mailer' => 'PHP/' .phpversion(),
+    'Mine-Version' => '1.0',
+    'Content-type' => 'text/html; charset=Utf-8'
+);
+
+//accusé de réception
+$headers .= "Disposition-Notification-To:$to";
+
+mail($to[0] && $to[1], $subject, $html,$headers, "-f Travelworld@gmail.com");
+
+if (mail($to[0] && $to [1],$subject, $html, $headers)) {
+    // Envoi du message
+
+    echo 'Votre message a bien été envoyé ';
+}
+else {
+    // Non envoyé
+    echo "Votre message n'a pas pu être envoyé";
+}
